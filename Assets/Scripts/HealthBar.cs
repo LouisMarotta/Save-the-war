3966b25d9bb7047dbb574a2f58a520bf52dccae3
Rhiss <rhiss@tutanota.com>
2021-02-26 12:14:06 +0100

    Sistema danno/Vita

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthSlider;
+
+    public void SetMaxHealth(int MaxHealth)
+    {
+        healthSlider.maxValue = MaxHealth;
+        healthSlider.value = MaxHealth;
+    }
+
+    public void SetHealth(int Health) 
+    {
+        healthSlider.value = Health;
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 5;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        //Intitialize currenthealth with max health value
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {   /*
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(1);
+        }
+        */
+    }
+
+    void TakeDamage(int damage)
+    {
+        Debug.Log($"Took {damage} damage");
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    void Heal(int heal)
+    {
+        currentHealth += heal;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    bool isDead()
+    {
+        return currentHealth == 0;
+    }
+}
