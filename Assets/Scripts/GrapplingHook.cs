4cd95e6fa15cd72793ca1eaeb74ba745919396eb
Leonardo Grimaldi <leonardo22grimaldi@gmail.com>
2021-02-26 10:42:26 +0100

    Grappling hook

---
+++ Assets/Scripts/GrapplingHook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingHook : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Vector2 Direction;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Direction = MousePos - (Vector2)transform.position;
+
+        FaceMouse();
+    }
+
+    void FaceMouse()
+    {
+        transform.right = Direction;
+    }
+}
