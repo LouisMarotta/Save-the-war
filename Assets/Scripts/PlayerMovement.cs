315a60f4c3abc126fc29779e41d0fc4d48dc5b82
Rhiss <rhiss@tutanota.com>
2021-03-02 11:50:41 +0100

    final scenes/script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -50,62 +50,6 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
 
-        if (horizontalMove < 0f || horizontalMove > 0f)
-        {
-            //animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
-            //playerSprite.flipX = horizontalMove < 0f;
-            if (isSwinging)
-            {
-                //animator.SetBool("IsSwinging", true);
 
-                // 1 - Get a normalized direction vector from the player to the hook point
-                var playerToHookDirection = (ropeHook - (Vector2)transform.position).normalized;
-
-                // 2 - Inverse the direction to get a perpendicular direction
-                Vector2 perpendicularDirection;
-                if (horizontalMove < 0)
-                {
-                    perpendicularDirection = new Vector2(-playerToHookDirection.y, playerToHookDirection.x);
-                    var leftPerpPos = (Vector2)transform.position - perpendicularDirection * -2f;
-                    Debug.DrawLine(transform.position, leftPerpPos, Color.green, 0f);
-                }
-                else
-                {
-                    perpendicularDirection = new Vector2(playerToHookDirection.y, -playerToHookDirection.x);
-                    var rightPerpPos = (Vector2)transform.position + perpendicularDirection * 2f;
-                    Debug.DrawLine(transform.position, rightPerpPos, Color.green, 0f);
-                }
-
-                var force = perpendicularDirection * swingForce;
-                //rBody.AddForce(force, ForceMode2D.Force);
-            }
-            else
-            {
-                //animator.SetBool("IsSwinging", false);
-                if (groundCheck)
-                {
-                    var groundForce = speed * 2f;
-                    rBody.AddForce(new Vector2((horizontalMove * groundForce - rBody.velocity.x) * groundForce, 0));
-                    rBody.velocity = new Vector2(rBody.velocity.x, rBody.velocity.y);
-                }
-            }
-        }
-        else
-        {
-            //animator.SetBool("IsSwinging", false);
-            //animator.SetFloat("Speed", 0f);
-        }
-
-        if (!isSwinging)
-        {
-            if (!groundCheck) return;
-
-            isJumping = jumpInput > 0f;
-            if (isJumping)
-            {
-                rBody.velocity = new Vector2(rBody.velocity.x, jumpSpeed);
-            }
-        }
     }
 }
-
