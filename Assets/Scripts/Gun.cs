71336beeb42fccf67f57d01636a063b1dd0a1c26
LouisMarotta <38915875+LouisMarotta@users.noreply.github.com>
2021-03-10 11:19:40 +0100

    Gun Script

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    [Header("Main Camera:")]
+    public GameObject prefabCamera;
+
+    [Header("Transform Ref:")]
+    public Transform gunHolder;
+    public Transform gunPivot;
+    public Transform firePoint;
+
+    [Header("Bullet:")]
+    public GameObject bulletPrefab;
+
+    [Header("Rotation:")]
+    [SerializeField] private bool rotateOverTime = true;
+    [Range(0, 60)] [SerializeField] private float rotationSpeed = 4;
+
+    [SerializeField] private int weaponDamage = 1;
+
+    [Header("Effects:")]
+    [SerializeField] public LineRenderer lineRenderer;
+
+
+
+    private Camera m_camera;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("I have shot");
+            StartCoroutine(Shoot());
+        }
+    }
+
+
+    IEnumerator Shoot()
+    {
+        RaycastHit2D hitInfo;
+        hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+
+
+        if (hitInfo)
+        {
+            Debug.Log(hitInfo.transform.name);
+            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(weaponDamage);
+
+            }
+
+            //Hit VFX
+            //Instantiate(impactEffect, hitInfo.point, Quaternion.identity);
+
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+        }
+        else
+        {
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, firePoint.position + firePoint.right * 100);
+        }
+
+        lineRenderer.enabled = true;
+
+        yield return 0;
+
+        lineRenderer.enabled = false;
+    }
+
+
+    void RotateGun(Vector3 lookPoint, bool allowRotationOverTime)
+    {
+        Vector3 distanceVector = lookPoint - gunPivot.position;
+
+        float angle = Mathf.Atan2(distanceVector.y, distanceVector.x) * Mathf.Rad2Deg;
+        if (rotateOverTime && allowRotationOverTime)
+        {
+            gunPivot.rotation = Quaternion.Lerp(gunPivot.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * rotationSpeed);
+        }
+        else
+        {
+            gunPivot.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Rotate the gun
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+
+        if (rotationZ < -90 || rotationZ > 90)
+        {
+
+
+
+            if (gunHolder.transform.eulerAngles.y == 0)
+            {
+
+
+                transform.localRotation = Quaternion.Euler(180, 0, -rotationZ);
+
+
+            }
+            else if (gunHolder.transform.eulerAngles.y == 180)
+            {
+
+
+                transform.localRotation = Quaternion.Euler(180, 180, -rotationZ);
+
+
+            }
+
+        }
+
+    }
+}
+
+/*
+    private void OnDrawGizmosSelected()
+    {
+        if (firePoint != null && hasMaxDistance)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(firePoint.position, maxDistance);
+        }
+    }
+    */
+
