b0f809133f28c680ff66301e63fde85d7f458f1b
Leonardo Grimaldi <leonardo22grimaldi@gmail.com>
2021-04-14 21:17:01 +0200

    Animazioni + file background

---
+++ Assets/Scripts/background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background : MonoBehaviour
+{
+    public GameObject bg0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bg0 = gameObject.transform.Find("0").gameObject;
+        //bg0.transform.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
